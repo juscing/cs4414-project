95d7ccb2d204dbc329a7dc62e797ab98309621ad
juscing <juscing@gmail.com>
2014-04-16 19:43:38 -0400

    Added service java file. Configuring the manifest. Trying to avoid the bind_service_notification permission... I do not think we need it but we may

---
+++ android/src/edu/virginia/cs/va2j/SprebbleService.java
@@ -0,0 +1,20 @@
+package edu.virginia.cs.va2j;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class SprebbleService extends AccessibilityService {
+
+	@Override
+	public void onAccessibilityEvent(AccessibilityEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onInterrupt() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
